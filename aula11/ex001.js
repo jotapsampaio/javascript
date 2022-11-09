69c6ecb4f98a3bba972caa326e09c2b97af77d40
JP Sampaio <108679561+jotapsampaio@users.noreply.github.com>
2022-11-09 13:15:30 -0300

    ex002

---
+++ aula11/ex001.js
@@ -0,0 +1,6 @@
+var velocidade = 80.3;
+console.log(`A sua velocidade é de ${velocidade} km/h`);
+if (velocidade > 60){
+    console.log("Você ultrapassou a velocidade permitida, será MULTADA!!!");
+}
+console.log("Lembre-se de usar o cinto de segurança");
---
+++ aula11/ex002.js
@@ -0,0 +1,6 @@
+var pais = "Inglaterra";
+if (pais === "Brasil"){
+    console.log("Você é Brasileiro");
+}else{
+    console.log("Você é Estrangeiro");
+}
