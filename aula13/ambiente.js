8d0889f7dd804e420568595fd66dfa3f330b6d44
JP Sampaio <108679561+jotapsampaio@users.noreply.github.com>
2022-11-11 08:39:38 -0300

    while e do while

---
+++ aula13/ambiente.js
@@ -0,0 +1,24 @@
+//While é conhecido por fazer o teste no inicio do comando;
+
+let contador = 1;
+while (contador <= 8){
+    console.log(`Contando ${contador} vez(es)`);
+    contador++;
+}
+
+//do while primeiro faz o comando e depois faz o teste;
+
+let inicio = 1;
+do {
+    console.log(`Contou ${inicio} vez(es)`);
+    inicio++;
+} while(inicio <= 8);
+
+
+// console.log("Tudo bem?");
+// console.log("Tudo bem?");
+// console.log("Tudo bem?");
+// console.log("Tudo bem?");
+// console.log("Tudo bem?");
+// console.log("Tudo bem?");
+// console.log("Tudo bem?");
