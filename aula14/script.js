27dd94da80b0b3ad212d642b724fe1ab60635aab
JP Sampaio <108679561+jotapsampaio@users.noreply.github.com>
2022-11-11 14:25:00 -0300

    inicio e fim

--- aula14/script.js
+++ aula14/script.js
@@ -1,5 +1,7 @@
 //O for faz a inicialização, o teste lógico e o incremento tudo no mesmo comando;
 
+console.log("Inicio...");
 for(var contador = 0; contador <= 7; contador++){
     console.log(contador);
 }
+console.log("Fim!!!")
