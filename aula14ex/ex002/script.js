502bc5c5d92d6361b6ec09829736049ced940044
JP Sampaio <108679561+jotapsampaio@users.noreply.github.com>
2022-11-12 12:40:54 -0300

    segundo exercicio

--- aula14ex/ex002/script.js
+++ aula14ex/ex002/script.js
@@ -1,1 +1,20 @@
-alert("Olá");
+function tabuada(){
+    let numero = document.getElementById("numero")
+    let tabuada = document.getElementById("selTabuada")
+
+    if(numero.value.length == 0){
+        alert("Número inválido! Por favor tente novamente.")
+    }else{
+        let num = Number(numero.value)
+        let contador = 1;
+
+        tabuada.innerHTML = ""
+        while(contador <= 10){
+            let item = document.createElement("option")
+            item.text = `${num} x ${contador} = ${num*contador}`
+            item.value = `tab${contador}`
+            tabuada.appendChild(item)
+            contador++
+        }
+    }
+}
