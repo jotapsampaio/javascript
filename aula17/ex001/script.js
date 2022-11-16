c05a8849d937c309324cca40944216a646ca132c
JP Sampaio <108679561+jotapsampaio@users.noreply.github.com>
2022-11-16 20:51:13 -0300

    exercício utilizando fuction e arrays

---
+++ aula17/ex001/script.js
@@ -0,0 +1,65 @@
+let num = document.querySelector("#inum")
+let lista = document.querySelector("#select")
+let resultado = document.querySelector("#resposta") 
+let valores = []
+
+function isNumero(n){
+    if(Number(n) >= 1 && Number(n) <= 100){
+        return true
+    }else{
+        return false
+    }
+}
+
+function isLista(n, l){
+    if(l.indexOf(Number(n)) != -1){
+        return true
+    }else{
+        return false
+    }
+}
+
+function adicionar(){
+    if(isNumero(num.value) && !isLista(num.value, valores)){
+        valores.push(Number(num.value))
+        let item = document.createElement("option")
+        item.text = `Valor ${num.value} adicionado`
+        lista.appendChild(item)
+        resultado.innerHTML = ""
+    }else{
+        alert("Valor inválido ou já encontrado na lista!")
+    }
+    num.value = ""
+    num.focus()
+}
+
+function finalizar(){
+    if(valores.length == 0){
+        alert("Adicione valores antes de finalizar!")
+    }else{
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+
+        
+        for(let pos in valores){
+            soma += valores[pos]
+            if(valores[pos] > maior){
+                maior = valores[pos]
+            }
+            if(valores[pos] < menor){
+                menor = valores[pos]
+            }
+        }
+
+        media = soma / tot
+        resultado.innerHTML = ""
+        resultado.innerHTML += `<p> Ao todo temos ${tot} números cadastrados.<p>`
+        resultado.innerHTML += `<p> O maior valor informado foi ${maior}.<p>`
+        resultado.innerHTML += `<p> O menor valor informado foi ${menor}.<p>`
+        resultado.innerHTML += `<p> A soma dos valores foi ${soma}. <p>`
+        resultado.innerHTML += `<p> A media dos valores foi ${media}.<p>`
+    }
+}   
